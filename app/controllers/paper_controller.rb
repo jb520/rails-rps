31cec16f0d07ac3e6a64cc5db2b49a0b1142eece
Jennifer Rahman <81711306+jb520@users.noreply.github.com>
2024-06-04 17:15:46 +0000

    add functionality to rps choices

--- app/controllers/paper_controller.rb
+++ app/controllers/paper_controller.rb
@@ -1,5 +1,8 @@
 class PaperController < ApplicationController
   def render_paper
+    @player_choice = "paper"
+    comp_choices = ["rock","paper","scissors"]
+    @computer_choice = comp_choices.sample
     render({ :template => "game_templates/play_paper" })
   end
 end
--- app/controllers/scissors_controller.rb
+++ app/controllers/scissors_controller.rb
@@ -1,5 +1,8 @@
 class ScissorsController < ApplicationController
   def render_scissors
+    @player_choice = "scissors"
+    comp_choices = ["rock","paper","scissors"]
+    @computer_choice = comp_choices.sample
     render({ :template => "game_templates/play_scissors" })
   end
 end
